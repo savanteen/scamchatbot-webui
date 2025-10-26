2e5232b1fabbe91bd79b64396dedfc4fe3d53cdc
savanteen <dexter.tiah@gmail.com>
2025-10-26 16:24:47 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+  </head>
+  <body>
+    <df-messenger
+      intent="WELCOME"
+      chat-title="Scam Chatbot"
+      agent-id="persuasion-476305"
+      language-code="en"
+    ></df-messenger>
+  </body>
+</html>
